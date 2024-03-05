ff8f656f3456b9fec397db29c12a4d720ee9a199
savameta <152951323+savameta@users.noreply.github.com>
2024-03-05 13:32:40 +0700

    doc: update database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { EventsModule } from './events/events.module';
 import { RoomModule } from './room/room.module';
 import { MinigameModule } from './minigame/minigame.module';
+
+const database =
+  'mongodb+srv://spirity:roFYY7BoYRp9YT4n@minesweeper.q0mdgtc.mongodb.net/?retryWrites=true&w=majority&appName=minesweeper';
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://metaphor:metaphor@metaphor.lgztn.mongodb.net',
-      {
-        dbName: 'metaphor',
-      },
-    ),
+    MongooseModule.forRoot(database, {
+      dbName: 'minesweeper',
+    }),
     EventsModule,
     RoomModule,
     MinigameModule,
